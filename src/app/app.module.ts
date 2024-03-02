2ab9aa61e21cf953d0cfb9dd42e2539bac381751
Matias Balestra <mbalestra@applica.dev>
2024-03-02 17:01:38 -0300

    fin clase 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,20 @@ import { PagesModule } from './pages/pages.module';
 
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { AuthModule } from './auth/auth.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     NopagefoundComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     PagesModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
