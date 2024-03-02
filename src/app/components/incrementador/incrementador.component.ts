dee5098048abc72438bbe92a9c1e82260e3aa610
Matias Balestra <mbalestra@applica.dev>
2024-03-02 20:23:19 -0300

    fin 8 - input output

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,37 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
   templateUrl: './incrementador.component.html',
   styleUrls: ['./incrementador.component.css']
 })
-export class IncrementadorComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  progreso: number = 40;
+export class IncrementadorComponent {
 
 
-
-  get getPorcentaje() {
-    return `${this.progreso}%`;
-  }
+  @Input() progreso: number = 40;
+  @Output('valor') cambioValor: EventEmitter<number> = new EventEmitter();
 
   cambiarValor(valor: number) {
 
     if (this.progreso >= 100 && valor > 0) {
+      this.cambioValor.emit(100);
       this.progreso = 100;
       return;
     }
 
     if (this.progreso <= 0 && valor < 0) {
+      this.cambioValor.emit(0);
       this.progreso = 0;
       return;
     }
 
     this.progreso = this.progreso + valor;
+    this.cambioValor.emit(this.progreso);
   }
 }
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -10,6 +10,16 @@ import { Component, } from '@angular/core';
 
 export class ProgressComponent {
 
+  progreso1: number = 20;
+  progreso2: number = 40;
+
+  get getProgreso1() {
+    return `${this.progreso1}%`;
+  }
+
+  get getProgreso2() {
+    return `${this.progreso2}%`;
+  }
 
 
 }
