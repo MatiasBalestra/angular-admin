b147ab3651a07fbeca1ddf1864e7aa53adfb94f6
Matias Balestra <mbalestra@applica.dev>
2024-03-03 20:30:17 -0300

    clases css

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -9,21 +9,40 @@ import { Component, OnInit } from '@angular/core';
 export class AccountSettingsComponent implements OnInit {
 
   public linkTheme = document.querySelector('#theme');
+  public links: NodeListOf<Element>;
 
 
   constructor() { }
 
   ngOnInit(): void {
+    this.links = document.querySelectorAll('.selector');
   }
 
   changeTheme(theme: string) {
-    console.log(theme);
+
 
     const url = `./assets/css/colors/${theme}.css`;
 
     this.linkTheme?.setAttribute('href', url);
 
     localStorage.setItem('theme', url);
+
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme() {
+
+    this.links.forEach(elemen => {
+      elemen.classList.remove('working');
+      const btnTheme = elemen.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+
+      const currentTheme = this.linkTheme?.getAttribute('href');
+
+      if (btnThemeUrl === currentTheme) {
+        elemen.classList.add('working');
+      }
+    })
 
   }
 
