2c5d874aea7cb5336d9b111bdf90fc1d56b3e1aa
Matias Balestra <mbalestra@applica.dev>
2024-03-05 22:04:47 -0300

    rxjs video 10 completado

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, retry, interval } from 'rxjs';
+import { take, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -10,38 +11,56 @@ import { Observable } from 'rxjs';
 export class RxjsComponent implements OnInit {
 
   constructor() {
+    /* this.retornaObservable().pipe(
+      retry(2)
+    ).subscribe(
+      valor => valor == 3 ? console.log('toco 3') : console.log('subs', valor),
+      error => console.warn('Error', error),
+      () => console.info('Obs terminado')
+    ); */
+
+    this.retornaIntevalo().subscribe(console.log);
+  }
 
 
-    const obs$ = new Observable(observer => {
 
-      let i = -1;
+  retornaObservable(): Observable<number> {
+    let i = -1;
+
+    const obs$ = new Observable<number>(observer => {
+
       const intervalo = setInterval(() => {
+
         i++;
         observer.next(i);
+
+
+        if (i === 2) {
+          observer.error('i llego al valor de 2');
+        }
 
         if (i === 4) {
           clearInterval(intervalo);
           observer.complete();
         }
 
-        if (i === 2) {
-          observer.error('i llego al valor de 2');
-        }
 
       }
         , 1000)
 
-
-
     });
 
-    obs$.subscribe(
-      valor => console.log('subs', valor),
-      error => console.error('Error', error),
-      () => console.info('Obs terminado')
-    );
-
+    return obs$;
   }
+
+
+  retornaIntevalo(): Observable<string> {
+    return interval(1000).pipe(
+      take(4),
+      map(i => `${i + 1}   ${i + 2}`)
+    )
+  }
+
 
   ngOnInit(): void {
   }
